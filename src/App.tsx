0e83a10551c3242549f2b1b59f8b9dd5285fe685
jack-sparrow-1904 <jack.sparrow@necto.pro>
2025-06-08 10:37:48 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import ProtectedRoute from './components/ProtectedRoute';
-import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage';
-import DashboardPage from './pages/DashboardPage';
-import WeeklyReviewPage from './pages/WeeklyReviewPage';
-import { Toaster } from "@/components/ui/sonner"
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/" element={<ProtectedRoute />}>
-            <Route index element={<DashboardPage />} />
-            <Route path="weekly-review" element={<WeeklyReviewPage />} />
-          </Route>
-          <Route path="*" element={<Navigate to="/" />} /> 
-        </Routes>
-        <Toaster richColors theme="dark" />
-      </AuthProvider>
-    </Router>
+    <>
+      <div>Start prompting.</div>
+    </>
   );
 }
 
